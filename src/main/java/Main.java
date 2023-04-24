7a679945363e12192b9c75e8b77fdfa463945af4
refik17 <refik17@uw.edu>
2023-04-23 22:03:12 -0700

    m

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,20 @@
 public class Main {
     public static void main(String[] args) {
+        Cat coco = new Cat("coco", 14);
+        Dog max = new Dog("max", 5, "Refik");
+        Rabbit snowWhite = new Rabbit("Gangy",12, "blue");
+        Toy robot = new Toy("Terminator");
+
+        Toy car = new Toy("Lambo");
+
+        Action[] homeFriends = new Action[4];
+        homeFriends[0] = coco;
+        homeFriends[1] = max;
+        homeFriends[2] = snowWhite;
+        homeFriends[3] = car;
+
+
+        for (Action friend : homeFriends) {
+            friend.play();
     }
 }
